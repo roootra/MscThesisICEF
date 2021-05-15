0da65b37eec79131983161a2453ceb8cd0bcc64d
Artur Zmanovskii <anzmanovskii@gmail.com>
2021-05-15 20:09:26 +0500

    Data description tables are added

--- Scripts/SVAR_monthly_good_fin_workon.R
+++ Scripts/SVAR_monthly_good_fin_workon.R
@@ -3,8 +3,6 @@ library(rio)
 library(lubridate)
 library(bvarsv)
 library(VARsignR)
-library(bvartools)
-library(tsDyn)
 library(vars)
 library(tsDyn)
 library(ggplot2)
@@ -15,8 +13,7 @@ dat <- import("/Users/rutra/ВШЭ/Магистратура/Thesis/Data/Aggregat
 dat <- dat[NROW(dat):1,]
 
 #Deseasonalization
-to_deseasonalize <- c("ind_output_yoy", "imp_price_mom", "exp_price_mom",
-                      grep("cpi.*", colnames(dat), value=T))
+to_deseasonalize <- c("ind_output_yoy", grep("cpi.*", colnames(dat), value=T))
 dat_unseas <- dat
 for(colname in to_deseasonalize){
   current_ts <- ts(dat[,colname], start=c(2005, 3), frequency=12)
@@ -24,29 +21,21 @@ for(colname in to_deseasonalize){
                        #regression.aictest=NULL, outlier=NULL,
                        #automdl=NULL,
                        seats.noadmiss="yes")
-  print(colname)
-  print(summary(current_seas))
-  print("************************")
   dat_unseas[,colname] <- as.numeric(final(current_seas))
 }
 
-
-
 #Cholesky decomposition
-data_chol <- dat_unseas[,c("oil_USD", "exp_price_mom", 
-                           "neer_mom", "miacr_31", "gdp_per_cap_SA", "cpi_all_mom")]
+data_chol <- dat_unseas[,c("oil_USD", "imp_price_mom", 
+                           "miacr_31", "neer_mom", "gdp_per_cap_SA", "cpi_all_mom")]
 data_chol$neer_mom <- data_chol$neer_mom * -1
 VARselect(data_chol)$selection
-
-model_VAR <- TVAR(data_chol, include="const", model="TAR", lag=4)
-
 model_VAR <- VAR(data_chol, p = 4, type = "const")
 choldec <- id.chol(model_VAR)
 irf_choldec <- irf(choldec, n.ahead = 12, ortho=TRUE)
 plot(irf_choldec)
-#Export prices
-sum(irf_choldec$irf$`epsilon[ exp_price_mom ] %->% cpi_all_mom`) /
-  sum(irf_choldec$irf$`epsilon[ exp_price_mom ] %->% neer_mom`)
+#Import prices
+sum(irf_choldec$irf$`epsilon[ imp_price_mom ] %->% cpi_all_mom`) /
+  sum(irf_choldec$irf$`epsilon[ imp_price_mom ] %->% neer_mom`)
 #Oil
 sum(irf_choldec$irf$`epsilon[ oil_USD ] %->% cpi_all_mom`) /
   sum(irf_choldec$irf$`epsilon[ oil_USD ] %->% neer_mom`)
@@ -61,20 +50,12 @@ sum(irf_choldec$irf$`epsilon[ gdp_per_cap_SA ] %->% cpi_all_mom`) /
   sum(irf_choldec$irf$`epsilon[ gdp_per_cap_SA ] %->% neer_mom`)
 
 #Smooth transition
-st_exp_restrmat <- matrix(nrow=NCOL(data_chol), ncol=NCOL(data_chol))
-st_exp_restrmat[2:6,1] <- 0
-st_exp_restrmat[3:6,2] <- 0
-#diag(st_exp_restrmat) <- 1
-
-
-model_cv <- id.st(model_VAR, c_lower=0.05, c_upper=0.95, c_step=3, nc=8, c_fix=70,
-                  gamma_lower=-5, gamma_upper=5, gamma_step=0.02, 
-                  restriction_matrix=st_exp_restrmat) #70
+model_cv <- id.st(model_VAR, c_lower=0.05, c_upper=0.95, c_step=3, nc=8, c_fix=70) #70
 irf_cv <- irf(model_cv, n.ahead = 12, ortho=TRUE)
 plot(irf_cv)
-#Export price
-sum(irf_cv$irf$`epsilon[ exp_price_mom ] %->% cpi_all_mom`) /
-  sum(irf_cv$irf$`epsilon[ exp_price_mom ] %->% neer_mom`)
+#Import price
+sum(irf_cv$irf$`epsilon[ imp_price_mom ] %->% cpi_all_mom`) /
+  sum(irf_cv$irf$`epsilon[ imp_price_mom ] %->% neer_mom`)
 #Oil
 sum(irf_cv$irf$`epsilon[ oil_USD ] %->% cpi_all_mom`) /
   sum(irf_cv$irf$`epsilon[ oil_USD ] %->% neer_mom`)
@@ -88,3 +69,30 @@ sum(irf_cv$irf$`epsilon[ neer_mom ] %->% cpi_all_mom`) /
 sum(irf_cv$irf$`epsilon[ gdp_per_cap_SA ] %->% cpi_all_mom`) /
   sum(irf_cv$irf$`epsilon[ gdp_per_cap_SA ] %->% neer_mom`)
 
+corstarsl <- function(x){ 
+  require(Hmisc) 
+  x <- as.matrix(x) 
+  R <- rcorr(x)$r 
+  p <- rcorr(x)$P 
+  
+  ## define notions for significance levels; spacing is important.
+  mystars <- ifelse(p < .001, "***", ifelse(p < .01, "** ", ifelse(p < .05, "* ", " ")))
+  
+  ## trunctuate the matrix that holds the correlations to two decimal
+  R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1] 
+  
+  ## build a new matrix that includes the correlations with their apropriate stars
+  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x)) 
+  diag(Rnew) <- paste(diag(R), " ", sep="") 
+  rownames(Rnew) <- colnames(x) 
+  colnames(Rnew) <- paste(colnames(x), "", sep="") 
+  
+  ## remove upper triangle
+  Rnew <- as.matrix(Rnew)
+  Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
+  Rnew <- as.data.frame(Rnew) 
+  
+  ## remove last column and return the matrix (which is now a data frame)
+  Rnew <- cbind(Rnew[1:length(Rnew)-1])
+  return(Rnew) 
+}
